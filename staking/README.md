3f1235e6b3b5ad58f9bfd51589a4cf0457e60a7c
Brian Obot <brianobot@gmail.com>
2025-02-13 15:07:48 +0100

    Enhance staking program: update dependencies, add user registration functionality, and improve README documentation

--- staking/README.md
+++ staking/README.md
@@ -1,5 +1,11 @@
 # Staking
 This is basically the process of locking a token (any asset) with the intent of earning a reward.
 
+## Process Overview
+- There is one Config Account to handle the configuration for the staking protocol
+- there is one User Account for ever user that uses the staking protocol
+- Each time a user stakes an NFT, a stake account handles each NFT case
+ 
+
 ## HOME WORK
 unix time stamp vs clock as applied to the solana ecosystem
--- staking/programs/staking/src/contexts/stake.rs
+++ staking/programs/staking/src/contexts/stake.rs
@@ -1,25 +1,58 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{approve, Approve, FreezeAccount, Mint, Token, TokenAccount};
+use anchor_spl::{
+    metadata::{
+        mpl_token_metadata::instructions::{
+            FreezeDelegatedAccountCpi, FreezeDelegatedAccountCpiAccounts,
+        },
+        MasterEditionAccount, Metadata, MetadataAccount
+    },
+    token::{approve, Approve, Mint, Token, TokenAccount},
+};
 
 use crate::state::{UserAccountState, StakeAccountState, StakeConfig};
 use crate::error::StakeError;
 
+/*
+    One Majoy difference between staking and nft marketplace is that the nft is not transferred from the owners (holder's)
+    wallet, but while it is staked, it can not be transferred from the wallet of the holder
+
+*/
 
 #[derive(Accounts)]
 pub struct Stake<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
+    // this is needed because we need to know the nft the user is staking in the first place
+    // when interacting with any type of token, it is good practise to always require it mint for verification
+    // purposes
     pub nft_mint: Account<'info, Mint>,
 
     #[account(
         mut,
-        associated_token::mint = nft_mint,
-        associated_token::authority = user,
+        associated_token::mint = nft_mint, // accounts are treated as pubkey due to rust Deref<Target = Pubkey>
+        associated_token::authority = user, // same rules applies here
+        // so technically associated_token::authority = user.key() is still correct
     )]
+    // this is needed to store the NFT for the user, actually this is needed to hold the nft in question
     pub nft_mint_ata: Account<'info, TokenAccount>,
-    pub collection_mint: Account<'info, Mint>,
-    
-    pub metadata: Account<'info, MetadataAccount>,
+    // this is needed to verify the authenticty of the nft in question
+    pub collection_mint: Account<'info, Mint>, 
+    #[account(
+        // the Metadata account for all NFT as defined by metaplex must be derived from the ffs seeds
+        // "metadata", metadata_program_id and the mint address of the metadata account
+        seeds = [
+            b"metadata",
+            nft_mint.key().as_ref(),
+            metadata_program.key().as_ref(),
+        ],
+        bump,
+        // TODO: change the line below to metadata_program and see it the test still works
+        seeds::program = metadata_program.key(), // this tells anchor the use the metadata_program key to derive the address
+        // instead of the defaul Crate::ID which is our program id
+        constraint = metadata.collection.as_ref().unwrap().key().as_ref() == collection_mint.key().as_ref(),
+        constraint = metadata.collection.as_ref().unwrap().verified == true
+    )] // the seeds for the metadataAccount is predefined 
+    pub metadata: Account<'info, MetadataAccount>, // TODO: why do we need this
 
     #[account(
         seeds = [b"stake_config"],
