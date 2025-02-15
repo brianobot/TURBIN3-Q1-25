8a69a618f088e4d0d4ae088ff5efd79a27d2ddd5
Brian Obot <brianobot@gmail.com>
2025-02-15 17:26:03 +0100

    Enhance swap functionality: add mint_lp account and improve error handling for insufficient balance

--- amm-program/programs/amm-program/src/error.rs
+++ amm-program/programs/amm-program/src/error.rs
@@ -1,4 +1,6 @@
 use anchor_lang::error_code;
+use constant_product_curve::CurveError;
+// use solana_program::program_error::ProgramError;
 
 
 #[error_code]
@@ -13,4 +15,21 @@ pub enum AmmError {
     InsufficientTokenX,
     #[msg("Insufficien amount of token Y")]
     InsufficientTokenY,
+    #[msg("Insufficien Balance")]
+    InsufficientBalance,
+}
+
+// TODO: Understand how the From trait works in Rust
+impl From<CurveError> for AmmError {
+    fn from(error: CurveError) -> AmmError {
+        match error {
+            CurveError::InvalidPrecision => AmmError::InvalidAmount,
+            CurveError::Overflow => AmmError::InvalidAmount,
+            CurveError::Underflow => AmmError::InvalidAmount,
+            CurveError::InvalidFeeAmount => AmmError::InvalidAmount,
+            CurveError::InsufficientBalance => AmmError::InsufficientBalance,
+            CurveError::ZeroBalance => AmmError::InvalidAmount,
+            CurveError::SlippageLimitExceeded => AmmError::InvalidAmount,
+        }
+    }
 }
